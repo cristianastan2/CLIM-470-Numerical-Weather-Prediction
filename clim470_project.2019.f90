6d10eeb172a322b89a33349736adcbfd1e0924c3
cristianastan2 <31143522+cristianastan2@users.noreply.github.com>
2019-10-11 13:28:41 -0400

    Update clim470_project.2019.f90

--- clim470_project.2019.f90
+++ clim470_project.2019.f90
@@ -1,6 +1,8 @@
        program main
        implicit none
+       
+       integer::i,j,k
 
-       print*'CLIM 470 Final Class Project!!!'
+       print*,'CLIM 470 Final Class Project!!!'
 
        end program main
