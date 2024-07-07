237f5f76093b843da5f57f4e88e510edfc0dec8d
Cristiana Stan <cstan@gmu.edu>
2024-07-07 16:27:02 -0400

    Initial commit

--- Homework1/code1.py
+++ Homework1/code1.py
@@ -1,8 +1,1 @@
 print('Hello world')
-print('Hello world from master')
-print('Hello world from MyHomewor1')
-
-
-print('New code from master')
-print('New code from MyHomework1')
-print('More code from MyHomework1')
