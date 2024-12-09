3d6919b10fa0dcb1c944391dc8146edf5b95f169
Rachel L Davis <rdavis39@hop043.orc.gmu.edu>
2024-12-09 10:16:41 -0500

    Fixed several issues in class 12/9

--- Project/ModelREDO.f90
+++ Project/ModelREDO.f90
@@ -2,17 +2,18 @@ program lowresmodel
 implicit none
 
 !variable declaration
-real, parameter :: Lx = 6000, Ly = 2000                 !domain in km
-real, parameter :: Dx = 500                   !grid resolution in km
+integer, parameter :: Lx = 6000, Ly = 2000                 !domain in km
+integer, parameter :: Dx = 500                   !grid resolution in km
 real, parameter :: dT = 100                             !time step, 100 seconds
 integer, parameter :: Nx = (Lx/Dx), Ny = (Ly/Dx)        !create grid points for array
-integer :: i, j, t, n                !define other potentially important variables
+integer :: i, j, t, n, irec                !define other potentially important variables
 real :: time                                            !TIME
 
 real, dimension(Nx, Ny) :: h, u, v, q, hsurf, lq, zeta, ght, ken   !array creation with internal variables for each point
 real, dimension(Nx, Ny, 3) :: hu0, hv0, hq0, us0, vs0, h0 ! Variables for time differencing
 real, dimension(Nx, Ny) :: hu1, hu2, hu3, hv1, hv2, hv3, us1, us2, us3, vs1, vs2, vs3
 real, dimension(Nx, Ny, 3) :: alp0, bet0, gam0, del0, eps0, ken0, ght0, q0, z0, phi0
+real, dimension(Nx, Ny, 3) :: u0, v0
 
 !Variables for further time-stepping
 real, dimension(Nx, Ny) :: alp1, alp2, alp3, bet1, bet2, bet3
@@ -31,13 +32,23 @@ print *, "Y-Direction Grid Point #:", Ny
 
 ! Set initial conditions
 u(1:Nx,1:Ny) = 20 ! uniform zonal wind
-v(1:Nx,1) = 0.0
-v(1:Nx, Ny) = 0 ! rigid computational boundary
+u(1:Nx,1) = 0.0
+v(1:Nx, Ny) = 0.0 ! rigid computational boundary
 v(1:Nx,2:Ny-1) = 0.1
+
+!Just for writing out
+u0(:,:,1) = u
+v0(:,:,1) = v
+
+call initgrid(hsurf,Dx,Nx,Ny)
+
+print*, "Calculated hsurf"
 
 do i = 1,Nx
 	h(i,:) = 5000-hsurf(i, :) ! horizontal free surface
 end do
+
+
 
 !Initialize hu0 and hv0 arrays
 hu0(1,:,1) = (h(Nx,:) + h(2,:))/2.0
@@ -155,6 +166,8 @@ do n = 2, 3
 		end do
 	end do
 
+u0(:,:,n) = u
+v0(:,:,n) = v
 
 ! Update hu0 and hv0 for next time step 
 hu0(1,:,n) = (h(Nx,:) + h(2,:))/2.0
@@ -204,6 +217,9 @@ q0(:,:,n) = (f+z0(:,:,n))/hq0(:,:,n)
 ! Update ken0 for the next time step
 ken0(:,:,n)=(u(:,:)*u(:,:) + v(:,:)*v(:,:))/2
 
+
+end do ! Time loop 
+
 !Update previous values prior to next time step
 us1(:,:) = us0(:,:,1)
 us2(:,:) = us0(:,:,2)
@@ -245,52 +261,61 @@ ght1(:,:) = ght0(:,:,1)
 ght2(:,:) = ght0(:,:,2)
 ght3(:,:)= ght0(:,:,3)
 
+
+
+open(unit=10, file='topo.dat', access='direct', form='unformatted', status='unknown', action='write', recl=4*(Nx-2)*(Ny-2))  !scary thing
+write(10, rec=1) hsurf(2:Nx-1,2:Ny-1)
+
+open(unit=15, file='geopot.dat', access='direct', form='unformatted', status='unknown', action='write', recl=4*(Nx-2)*(Ny-2))
+irec=0
+do n=1,3
+	irec=irec+1
+	write(15, rec=irec) ght0(2:Nx-1,2:Ny-1,n)
 end do
 
-call initgrid(h, u, v, q, hsurf)
+open(unit=20, file='uwind.dat', access='direct', form='unformatted', status='unknown', action='write', recl=4*(Nx-2)*(Ny-2))
+irec=0 
+do n=1,3
+        irec=irec+1
+        write(20, rec=irec) u0(2:Nx-1,2:Ny-1,n)
+end do
 
-
-open(unit=10, file='topo.dat', access='direct', form='unformatted', status='unknown', action='write', recl=4*Nx*Ny)  !scary thing
-write(10, rec=1) hsurf
-
+open(unit=25, file='vwind.dat', access='direct', form='unformatted', status='unknown', action='write', recl=4*(Nx-2)*(Ny-2))
+irec=0 
+do n=1,3
+        irec=irec+1
+        write(25, rec=irec) v0(2:Nx-1,2:Ny-1,n)
+end do
 
 end program lowresmodel
 
 
 
-subroutine initgrid(h, u, v, q, hsurf)	!creates grid, ridge, and topography.
-implicit none
+subroutine initgrid(hsurf, Dx, Nx, Ny)	!creates grid, ridge, and topography.
+    implicit none
+  
+    integer, parameter :: Lx = 6000, Ly = 2000
+    integer, intent(in) :: Dx, Nx, Ny 
+   
 
-real, parameter :: Lx = 6000, Ly = 2000
-real, parameter :: Dx = 500
-integer, parameter :: Nx = (Lx/Dx), Ny = (Ly/Dx)
+    real, dimension(Nx,Ny), intent(out) :: hsurf
+    integer :: i, j
+  
+    print*, "hsurf =", hsurf
+    hsurf(:,:)=0.0
+    print*, "Dx =", Dx 
 
-real, dimension(Nx,Ny), intent(out) :: h, u, v, q, hsurf
-integer :: i, j
+    !grid resolution thing (topography)
 
-hsurf(:,:)=0
-
-!grid resolution thing (topography) NEEDS ADJUSTMENT BASED ON PREVIOUS MISTAKE
-if (Dx==500) then 
-	hsurf(Nx/2,:)=2000
-
-elseif (Dx==250) then
-	hsurf(Nx/2-1,:)=1000
-	hsurf(Nx/2,:)=2000
-	hsurf(Nx/2+1,:)=1000
-
-elseif (Dx==125) then
-	hsurf(Nx/2-2,:)=500
-	hsurf(Nx/2-1,:)=1000
-	hsurf(Nx/2,:)=2000
-	hsurf(Nx/2+1,:)=1000
-	hsurf(Nx/2+2,:)=500
-endif
+    hsurf((Nx)/2 + 1,:) = 2000
+    if (Dx==250) then
+        hsurf((Nx)/2,:) = 1000
+        hsurf((Nx)/2 + 2,:) = 1000
+    endif
+    if (Dx==125) then
+        hsurf((Nx)/2 - 1,:) = 500
+        hsurf((Nx)/2 + 3,:) = 500
+    endif
 	
-!initial conditions set to 0
-h = 0
-u = 20
-v = 0 ! needs to be changed
-q = 0
 
-end subroutine initgrid
+    end subroutine initgrid
